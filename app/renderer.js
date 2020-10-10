dc901040c24bd9fb0868c3acf952ce6bc4eff7fd
Chris Corday <corday@gmail.com>
2020-10-10 12:56:14 +0100

    [CB-6] Created menu items and style

--- app/renderer.js
+++ app/renderer.js
@@ -2,12 +2,44 @@ const Cookbook = require('../function/cookbook.js')
 
 const cookbook = new Cookbook();
 
+function createBrowseRecipe(recipe) {
+    // Creates div to hold recipe image and name. Sets up click event listener to handle loading recipe details.
+    let div = document.createElement("div")
+    div.className = 'recipe'
+    div.addEventListener("click", function () {
+        console.log(`${div.textContent} was clicked`)
+    })
+
+    // Create image of recipe element
+    let recipeImg = document.createElement("img")
+    recipeImg.className = 'recipeImgMenu'
+    recipeImg.alt = recipe.name
+    recipeImg.src = recipe.imagePath
+
+    // Create item label
+    let recipeLabel = document.createElement("span")
+    recipeLabel.className = 'recipeLabelMenu'
+    let recipeText = document.createTextNode(recipe.name)
+    recipeLabel.appendChild(recipeText)
+
+    div.appendChild(recipeImg)
+    div.appendChild(recipeLabel)
+
+    document.getElementById("menu-container").appendChild(div)
+}
+
+
+function removeSpinner() {
+    let spinner = document.getElementById("loader")
+    if (spinner !== undefined) {
+        spinner.parentNode.removeChild(spinner)
+    }
+}
+
 cookbook.loadRecipes().then(() => {
     cookbook.recipes.forEach(recipe => {
-        let tag = document.createElement("p");
-        let text = document.createTextNode(recipe.name);
-        tag.appendChild(text);
-        let element = document.getElementsByTagName("BODY")[0];
-        element.appendChild(tag);
+        createBrowseRecipe(recipe)
     })
+    removeSpinner()
 })
+
