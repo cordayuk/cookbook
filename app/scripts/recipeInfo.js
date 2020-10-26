698fbd0bb9ec7d17354861463529cc88abc96444
Chris Corday <corday@gmail.com>
2020-10-26 12:48:10 +0000

    [CB-7] Tided up recipe information elements creation.

--- app/scripts/recipeInfo.js
+++ app/scripts/recipeInfo.js
@@ -1,10 +1,10 @@
+const contentContainer = document.getElementById('main-content')
+
+/**
+ * Creates and adds the recipe information elements to the main content.
+ * @param recipe - the recipe to produce the recipe information pages for.
+ */
 function viewRecipeInfo(recipe) {
-    let recipeContainer = document.createElement('div')
-    recipeContainer.className = 'recipe-info-container'
-
-    let upperContainer = document.createElement('div')
-    upperContainer.className = 'recipe-upper-container'
-
     let header = document.createElement("h2")
     header.className = 'recipe-title'
     header.textContent = recipe.name
@@ -21,23 +21,33 @@ function viewRecipeInfo(recipe) {
     let cookingTime = document.createElement('span')
     cookingTime.textContent = `Cooking Time: ${recipe.cookingTime}`
 
-    let ingredientsContainer = document.createElement('div')
-    ingredientsContainer.className = 'ingredients'
+    let ingredientsContainer = createIngredientsContainer(recipe)
 
-    let ingredientsTitle = document.createElement('h3')
-    ingredientsTitle.textContent = 'Ingredients:'
+    let methodContainer = createMethodContainer(recipe)
 
-    let ingredientsList = document.createElement('ul')
+    let recipeContainer = document.createElement('div')
+    recipeContainer.className = 'recipe-info-container'
 
-    recipe.ingredients.forEach(ingredient => {
-        let ingredientItem = document.createElement('li')
-        ingredientItem.textContent = ingredient
+    let upperContainer = document.createElement('div')
+    upperContainer.className = 'recipe-upper-container'
 
-        ingredientsList.appendChild(ingredientItem)
-    })
+    let lowerContainer = document.createElement('div')
+    lowerContainer.className = 'recipe-lower-container'
 
-    ingredientsContainer.append(ingredientsTitle, ingredientsList)
+    upperContainer.append(image, servings, cookingTime)
+    lowerContainer.append(ingredientsContainer, methodContainer)
 
+    recipeContainer.append(header, upperContainer, lowerContainer)
+
+    contentContainer.appendChild(recipeContainer)
+}
+
+/**
+ * Populates and creates the method section of a recipe and returns a div container element.
+ * @param recipe - the recipe extract the method from.
+ * @returns {HTMLDivElement} div with classname method-container.
+ */
+function createMethodContainer(recipe) {
     let methodContainer = document.createElement('div')
     methodContainer.className = 'method-container'
 
@@ -54,12 +64,33 @@ function viewRecipeInfo(recipe) {
     })
 
     methodContainer.append(methodTitle, methodList)
+    return methodContainer
+}
 
-    upperContainer.append(header, image, servings, cookingTime, ingredientsContainer, methodContainer)
+/**
+ * Creates and populates the ingredients container for the inputed recipe.
+ * @param recipe - the recipe to extract the ingredients from.
+ * @returns {HTMLDivElement} - a div with classname ingredients-container
+ */
+function createIngredientsContainer(recipe) {
+    let ingredientsContainer = document.createElement('div')
+    ingredientsContainer.className = 'ingredients-container'
 
-    let contentContainer = document.getElementById('main-content')
+    let ingredientsTitle = document.createElement('h3')
+    ingredientsTitle.textContent = 'Ingredients:'
 
-    contentContainer.appendChild(upperContainer)
+    let ingredientsList = document.createElement('ul')
+
+    recipe.ingredients.forEach(ingredient => {
+        let ingredientItem = document.createElement('li')
+        ingredientItem.textContent = ingredient
+
+        ingredientsList.appendChild(ingredientItem)
+    })
+
+    ingredientsContainer.append(ingredientsTitle, ingredientsList)
+
+    return ingredientsContainer
 }
 
 module.exports = {
